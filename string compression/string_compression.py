96a54f807079f2b3f234b4fa31374c7643a044f0
pjunes <qwt1597@me.com>
2022-06-29 11:11:06 +0900

    string compression ver 1.1 flexible complete

--- string compression/string_compression.py
+++ string compression/string_compression.py
@@ -3,9 +3,11 @@
 
 def solution(s):
     # answer : min length of result
-    answer = 0
+    # answer = 0
     # result : result of compression
     result = ""
+    # result_hist : history of len(result)
+    result_len_hist = list()
 
     # s_len : length of input {s}
     s_len = len(s)
@@ -20,74 +22,70 @@ def solution(s):
     jump_cnt = 0
 
     while node_len <= int(s_len / 2):
+        ### initialize
+        result = ""
 
         for i in range(s_len):
-            # initialize
+            ### initialize
             r_cnt = 0
 
-            # jump stage
-
-            # case start.
-            # set start point and s_node
+            ## jump stage
+            if jump_cnt:
+                # print("jump")
+                jump_cnt -= 1
+                continue
 
             # Every points can be the start point. regardless of node_len
+            # exclude the remainder
             if i + node_len > s_len:
                 # debugging
                 # print(f"i{i} + node_len{node_len} < s_len{s_len}")
                 continue
 
+            ### case start.
+            ## set start point and s_node
+
             # s_node : compression unit
             s_node = s[i : i+node_len]
 
-            # debugging
-#             print(f"""
-# node_len : {node_len}
-# s_node   : {s_node}
-# i        : {i}
-# """)
-
+            # compare s_node with next node
             for j in range(i+node_len, s_len, node_len):
                 if s[j : j+node_len] == s_node:
                     r_cnt += 1
                 else:
                     break
 
-            # jump_cnt *= node_len
-
-            # string comparison in python is nothing special.
-            # if s_node == prev or not r_cnt:
-            #     r_cnt += 1
-            # else:
-            #     result += f"{r_cnt if r_cnt > 1 else None}{prev}"
-            #     r_cnt = 0
-            # prev = s_node
-
-            print(f"""
-node_len : {node_len}
-s_node   : {s_node}
-i        : {i}
-r_cnt    : {r_cnt}
-""")
+            if r_cnt:
+                result += f"{r_cnt + 1}{s_node}"
+            else:
+                result += f"{s_node[0]}"
 
             # jump stage setting
             # python에서는 for 문의 i 값을 바꿔줘도 loop의 횟수가 변하지 않기 때문에
             # 건너 뛰어야 할 경우를 구현, jump stage
-            jump_cnt = r_cnt * node_len
+            jump_cnt = ((node_len - 1) if r_cnt else 0) + r_cnt * node_len
+
+#             print(f"""
+# node_len : {node_len}
+# s_node   : {s_node}
+# i        : {i}
+# r_cnt    : {r_cnt}
+# jump_cnt : {jump_cnt}
+# """)
 
         # end of for
-
+        # print(result)
+        result_len_hist.append(len(result))
         
         node_len += 1
     # end of while
 
-
-
-
+    # print(min(result_len_hist))
     # return answer
-    return result
+    return min(result_len_hist)
 
 if __name__ == "__main__":
-    print(solution("aabbaccc"))
+    print(solution("abcabcdede"))
 
 
 # grave
@@ -104,4 +102,31 @@ if __name__ == "__main__":
     #         r_cnt = 0
     #     prev = c
 
+    
+
+            # jump_cnt *= node_len
+
+            # string comparison in python is nothing special.
+            # if s_node == prev or not r_cnt:
+            #     r_cnt += 1
+            # else:
+            #     result += f"{r_cnt if r_cnt > 1 else None}{prev}"
+            #     r_cnt = 0
+            # prev = s_node
+
 """
+
+# debugging
+#             print(f"""
+# node_len : {node_len}
+# s_node   : {s_node}
+# i        : {i}
+# """)
+
+#             print(f"""
+# node_len : {node_len}
+# s_node   : {s_node}
+# i        : {i}
+# r_cnt    : {r_cnt}
+# jump_cnt : {jump_cnt}
+# """)
