0ba2e2877ad944cfd965ac8641aafb85498b9181
pjunes <qwt1597@me.com>
2022-07-19 10:41:45 +0900

    numeric strings ver 1.2 complete + model

--- Numeric strings/numeric_strings.py
+++ Numeric strings/numeric_strings.py
@@ -1,9 +1,20 @@
-def solution(s): # s type:string
-    answer_string = "" # type:string
-    answer = 0 # type:int
+def solution(s):  # s type:string
+    answer_string = ""  # type:string
+    answer = 0  # type:int
 
     # match numeric string with integer
-    num_box = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"} # type:dict
+    num_box = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }  # type:dict
 
     temp = ""
     for c in s:
@@ -15,30 +26,43 @@ def solution(s): # s type:string
             if temp in num_box.keys():
                 answer_string += num_box[temp]
                 # print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
-                temp = "" # initialize temp
+                temp = ""  # initialize temp
 
-    answer = int(answer_string) # change answer_string(type:string) to answer(type:int)
+    # change answer_string(type:string) to answer(type:int)
+    answer = int(answer_string)
     return answer
+
 
 if __name__ == "__main__":
     sample = "one4seveneight"
     answer = solution(sample)
     print(answer)
 
-    ## old
-    # if len(temp) >= 1:
-        #     print(temp)
-        #     if temp in num_box.keys():
-        #         answer_string += num_box[temp]
-        #         print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
-        #         temp = "" # initialize temp
-        #     else:
-        #         temp += c
-        #         continue
+## old incomplete
+# if len(temp) >= 1:
+#     print(temp)
+#     if temp in num_box.keys():
+#         answer_string += num_box[temp]
+#         print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
+#         temp = "" # initialize temp
+#     else:
+#         temp += c
+#         continue
 
-        # if c.isnumeric():
-        #     answer_string += c
-        #     print(f"add {c}\n answer : {answer_string}")
-        #     continue
-        # else:
-        #     temp += c
+# if c.isnumeric():
+#     answer_string += c
+#     print(f"add {c}\n answer : {answer_string}")
+#     continue
+# else:
+#     temp += c
+
+#  model 1
+"""
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
+"""
