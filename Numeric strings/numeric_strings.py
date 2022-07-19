6cdbe1562e87e14c34376632dbbbb9d714ba30e5
pjunes <qwt1597@me.com>
2022-07-19 10:36:19 +0900

    numeric strings ver 1.1 complete

--- Numeric strings/numeric_strings.py
+++ Numeric strings/numeric_strings.py
@@ -1,33 +1,44 @@
-def solution(s): # s : string
-    answer_string = ""
-    answer = 0 # answer : int
+def solution(s): # s type:string
+    answer_string = "" # type:string
+    answer = 0 # type:int
 
-    num_box = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    # match numeric string with integer
+    num_box = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"} # type:dict
 
     temp = ""
     for c in s:
-        print(f"c : {c} / temp : {temp}")
-        if len(temp) >= 1:
-            print(temp)
-            if temp in num_box.keys():
-                answer_string += num_box[temp]
-                print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
-                temp = "" # initialize temp
-            else:
-                temp += c
-                continue
-
+        # print(f"c : {c} / temp : {temp}")
         if c.isnumeric():
             answer_string += c
-            print(f"add {c}\n answer : {answer_string}")
-            continue
         else:
             temp += c
+            if temp in num_box.keys():
+                answer_string += num_box[temp]
+                # print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
+                temp = "" # initialize temp
 
-    answer = int(answer_string)
+    answer = int(answer_string) # change answer_string(type:string) to answer(type:int)
     return answer
 
 if __name__ == "__main__":
     sample = "one4seveneight"
     answer = solution(sample)
     print(answer)
+
+    ## old
+    # if len(temp) >= 1:
+        #     print(temp)
+        #     if temp in num_box.keys():
+        #         answer_string += num_box[temp]
+        #         print(f"add {temp} => {num_box[temp]}\n answer : {answer_string}")
+        #         temp = "" # initialize temp
+        #     else:
+        #         temp += c
+        #         continue
+
+        # if c.isnumeric():
+        #     answer_string += c
+        #     print(f"add {c}\n answer : {answer_string}")
+        #     continue
+        # else:
+        #     temp += c
