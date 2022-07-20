e7bfe08090087ae1ed280c2f4451cfa63d934b19
pjunes <qwt1597@me.com>
2022-07-20 16:43:47 +0900

    parking fee calculation ver 1.0

---
+++ Parking fee calculation/parking_fee_calculation.py
@@ -0,0 +1,17 @@
+def solution(fees, records):
+    # fees type:list with integer [basic time, basic fee, unit time, unit fee]
+    # 1 ≤ fees[0] ≤ 1439, 0 ≤ fees[1] ≤ 100000, 1 ≤ fees[2] ≤ 1439, 1 ≤ fees[3] ≤ 10000
+
+    # records type:list with string ["시각 차량번호 내역", ... ]
+    # 1 ≤ len(records) ≤ 1000
+    # 시각 HH:MM은 00:00부터 23:59까지
+    # 차량번호는 자동차를 구분하기 위한, `0'~'9'로 구성된 길이 4인 문자열
+    # 내역은 길이 2 또는 3인 문자열로, IN 또는 OUT
+
+    # 차량 번호가 작은 자동차부터 청구할 주차 요금을 차례대로 정수 배열에 담아서 return
+    answer = []  # type:list with integer
+
+    
+
+
+    return answer
