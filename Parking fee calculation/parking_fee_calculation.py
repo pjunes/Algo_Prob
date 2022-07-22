55f0e9ab13e3dc95f748a7282a352c3fe6578f83
pjunes <qwt1597@me.com>
2022-07-22 10:31:30 +0900

    parking fee calculation ver 1.1

--- Parking fee calculation/parking_fee_calculation.py
+++ Parking fee calculation/parking_fee_calculation.py
@@ -11,7 +11,76 @@ def solution(fees, records):
     # 차량 번호가 작은 자동차부터 청구할 주차 요금을 차례대로 정수 배열에 담아서 return
     answer = []  # type:list with integer
 
-    
+    basic_time = fees[0]
+    basic_fee = fees[1]
+    unit_time = fees[2]
+    unit_fee = fees[3]
 
+    access_record = dict() # key serial : value in time
+    fee_record = dict() # key serial : value fee
+    status_record = dict() # key serial : value in(True)\out(False)
+
+    for record in records: # record type:string "time serial in/out"
+        time_s, serial, detail = record.split(' ')
+        time = int(time_s[:2]) * 60 + int(time_s[3:]) # conversion "00:00" to 000 minutes (integer)
+
+        if detail == "IN":
+            access_record[serial] = time
+            status_record[serial] = [True, time]
+        else: # if detail == "out":
+            unit = max((time - access_record[serial]) - basic_time, 0) / unit_time
+
+            # ceil unit
+            if unit%1:
+                unit = int(unit) + 1
+            else:
+                unit = int(unit)
+
+            fee_record[serial] = fee_record.get(serial, 0) + basic_fee + unit * unit_fee
+            
+            status_record[serial][0] = False
+
+        pass
+
+    for item in status_record.items():
+        if item[1][0] == True:
+            unit = max(((23*60+59) - item[1][1]) - basic_time, 0) / unit_time
+
+            # ceil unit
+            if unit%1:
+                unit = int(unit) + 1
+            else:
+                unit = int(unit)
+
+            fee_record[serial] = fee_record.get(serial, 0) + basic_fee + unit * unit_fee
+            
+            status_record[serial][0] = False
+
+    fee_record_sorted = sorted(fee_record.items())
+    
+    for i in range(len(fee_record_sorted)):
+        answer.append(fee_record_sorted[i][1])
 
     return answer
+
+if __name__ == "__main__":
+#     a = 2.1
+#     b = 2
+
+#     print("a는 ",end="")
+#     if (a%1):
+#         print("유리수")
+#     else:
+#         print("정수")
+
+#     print("b는 ",end="")
+#     if (b%1):
+#         print("유리수")
+#     else:
+#         print("정수")
+#     print(a%1)
+#     print(b%1)
+    sample_fees = [180, 5000, 10, 600]
+    sample_records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
+    answer = solution(sample_fees, sample_records)
+    print(answer)
