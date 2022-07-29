ab55ef96806ab5eac2a8b2b52fc953b769dea502
pjunes <qwt1597@me.com>
2022-07-29 17:43:33 +0900

    Sugar delivery ver 1.1 complete

--- BOJ/2839_sugar_delivery.py
+++ BOJ/2839_sugar_delivery.py
@@ -1,15 +1,22 @@
 def solution(N):
     answer = 0
-    answer += N//5
-    # N = N%5
-    answer += (N%5)//3
-    if (N%5)%3:
-        return -1
+    quotient_N5 = N//5
+    # remainder_N5 = N%5
+    for i in range(quotient_N5, -1, -1):
+        if (N-5*i)%3:
+            continue
+        else:
+            answer += i + (N-5*i)//3
+            return answer
 
-    return answer
+    return -1
 
 if __name__ == "__main__":
-    while(True):
-        N = int(input())
-        answer = solution(N)
-        print(answer)
+    N = int(input())
+    answer = solution(N)
+    print(answer)
+
+    # while(True):
+    #     N = int(input())
+    #     answer = solution(N)
+    #     print(answer)
