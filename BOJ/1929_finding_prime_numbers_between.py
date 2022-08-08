b64d9acca844eb7ffb277f0a600a4ce376fb97f1
pjunes <qwt1597@me.com>
2022-08-08 18:16:31 +0900

    Finding Prime Numbers Between Two Numbers ver 1.1

--- BOJ/1929_finding_prime_numbers_between.py
+++ BOJ/1929_finding_prime_numbers_between.py
@@ -1,3 +1,49 @@
+def prime_numbers_until_the_number(N):
+    prime_ar = [2]
+
+    prime_flag = True
+
+    for i in range(3, N+1):
+        prime_flag = True
+        for prime_num in prime_ar:
+            if not i%prime_num:
+                prime_flag = False
+                break
+
+        if prime_flag:
+            prime_ar.append(i)
+
+    return prime_ar
+
+def solution(M, N, prime_ar):
+    answer_ar = [n for n in range(M, N+1)]
+    # print(f"initial answer_ar : {answer_ar}")
+
+    for p in prime_ar:
+        for n in answer_ar:
+            if n == p:
+                continue
+            if n%p == 0:
+                # print(f"remove {n} / p : {p}, n : {n}, n%p : {n%p}")
+                answer_ar.remove(n)
+
+    return answer_ar
+
+if __name__ == "__main__":
+
+    M, N = map(int, input().split(" "))
+
+    prime_ar = prime_numbers_until_the_number(N) # N 이하의 모든 소수를 포함하는 ar return.
+    # print(f"prime_ar : {prime_ar}")
+
+    answer_ar = solution(M, N, prime_ar)
+    # print(f"answer_ar : {answer_ar}")
+
+    for answer in answer_ar:
+        print(answer)
+
+    pass
+
 """
 def solution(M, N):
     answer_ar = []
